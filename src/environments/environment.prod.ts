476a32bcb3d9605a7980b641df6348c3825080a3
Abel Lopes <abellopes@gmail.com>
2018-03-21 14:48:37 -0300

    wow

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCsj8CfhlAXOCbg6jXb8gZgsTBShGxxkA4',
+    authDomain: 'ponto-online-75ddf.firebaseapp.com',
+    databaseURL: 'https://ponto-online-75ddf.firebaseio.com',
+    projectId: 'ponto-online-75ddf',
+    storageBucket: 'ponto-online-75ddf.firebaseapp.com',
+    messagingSenderId: '1050577519688'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCsj8CfhlAXOCbg6jXb8gZgsTBShGxxkA4',
+    authDomain: 'ponto-online-75ddf.firebaseapp.com',
+    databaseURL: 'https://ponto-online-75ddf.firebaseio.com',
+    projectId: 'ponto-online-75ddf',
+    storageBucket: '',
+    messagingSenderId: '1050577519688'
+  }
 };
